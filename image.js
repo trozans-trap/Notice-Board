c40fbfe895f8c89f7dd9e88c800b857f9854b32c
trozans-trap <53917115+trozans-trap@users.noreply.github.com>
2020-02-07 11:38:11 +0530

    B

--- image.js
+++ image.js
@@ -2,11 +2,6 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-aws.config.update({
-    secretAccessKey:'ivbW/f2VTH3KDBQdwiDBQZ8szEVYv9Ae5tmcnw4r',
-    accessKeyId: 'AKIAJOYHBW4A7XLGKDAA',
-    region: 'us-east-2'
-});
 
 const s3 = new aws.S3();
  
